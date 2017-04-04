de471480e79f5f0b5dfe09c833e5ace4bdbb48af
Robert Hetzler <robert@hetzler.ca>
2017-04-03 18:28:46 -0700

    Checkpointing this work before i destroy it accidentally

--- lib/multitenancy/tenant_repo.ex
+++ lib/multitenancy/tenant_repo.ex
@@ -1,133 +1,196 @@
 defmodule Multitenancy.TenantRepo do
+    @moduledoc """
+    Yo Dawg. I heard you liked macros,
+     so i wrote a macro that writes macros which splice macros into other macros.
+    hope you like it.
 
-    # various ways in which we annotate the inputs to enforce the tenant schema
-    def tenant_annotated_query(query, conn, transform) do
-        query |> Ecto.Queryable.to_query |> Map.put(:prefix, transform.(conn) )
-    end
-    def tenant_annotated_changeset(changeset , conn, transform) do
-        %Ecto.Changeset{changeset | data: changeset.data |> Ecto.put_meta(prefix: transform.(conn)) }
-    end
-    def tenant_annotated_struct(struct, conn, transform) do
-        struct |> Ecto.put_meta(prefix: transform.(conn))
-    end
+    Objective: we want to create a TenantRepo wrapper inside our application which delegates
+     to the standard application Ecto.Repo but adds the behaviour of transparently specifying the
+     tenant prefix so the developer isn't burdened with this task (and prevents bugs... theoretically)
 
-    # some helpers for inputs which may vary...
-    def tenant_annotated_changeset_or_struct(%Ecto.Changeset{} = changeset , conn, transform) do
-        tenant_annotated_changeset(changeset, conn, transform)
-    end
-    # If it's not a changeset, assume it's a data struct
-    def tenant_annotated_changeset_or_struct(struct, conn, transform) do
-        tenant_annotated_struct(struct, conn, transform)
-    end
+    Standard query looks like this:
 
-    defmacro __using__(opts) do
-        quote bind_quoted: [opts: opts] do
+    Repo.all( Model )
 
-        @repo Keyword.fetch!(opts, :repo)
-        @transform Keyword.fetch!(opts, :transform)
+    in order to specifiy prefix, we have to annotate the query ( Model ) with the prefix:
 
-        defmacro all(queryable, opts \\ []) do
-            quote do
-                unquote(@repo).all(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
-        defmacro get(queryable, id, opts \\ []) do
-            quote do
-                unquote(@repo).get(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(id), unquote(opts))
-            end
-        end
-        defmacro get!(queryable, id, opts \\ []) do
-            quote do
-                unquote(@repo).get!(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(id), unquote(opts))
-            end
-        end
-        defmacro get_by(queryable, clauses, opts \\ []) do
-            quote do
-                unquote(@repo).get_by(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(clauses), unquote(opts))
-            end
-        end
-        defmacro get_by!(queryable, clauses, opts \\ []) do
-            quote do
-                unquote(@repo).get_by!(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(clauses), unquote(opts))
-            end
-        end
-        defmacro one(queryable, opts \\ []) do
-            quote do
-                unquote(@repo).one(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
-        defmacro one!(queryable, opts \\ []) do
-            quote do
-                unquote(@repo).one!(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
+     Model |> Ecto.Queryable.to_query |> Map.put(:prefix, "tenant_schema_prefix")
 
-        defmacro aggregate(queryable, aggregate, field, opts \\ []) do
-            quote do
-                unquote(@repo).aggregate(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(aggregate), unquote(field), unquote(opts))
-            end
-        end
+    but this adds an immense burden of room for error, as the patterns diverge from Phoenix generation boilerplate.
+    Instead we want to provide the ability to do this:
 
-        # does struct annotation need to be applied to "updates"
-        defmacro update_all(queryable, updates, opts \\ []) do
-            quote do
-                unquote(@repo).update_all(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(updates), unquote(opts))
-            end
-        end
-        defmacro delete_all(queryable, opts \\ []) do
-            quote do
-                unquote(@repo).delete_all(unquote(queryable) |> Multitenancy.TenantRepo.tenant_annotated_query(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
+    TenantRepo.all( Model )
+    # which would transparently bake in the necessary schema prefix.
+    # However, that prefix is dynamic per connection, so using a precompiled module is not viable.
+
+    # instead, this approach seeks to provide a set of macros which accomplishes this.
 
 
-        defmacro insert(struct, opts \\ []) do
-            quote do
-                unquote(@repo).insert(unquote(struct) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
-        defmacro insert!(struct, opts \\ []) do
-            quote do
-                unquote(@repo).insert!(unquote(struct) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
+    TenantRepo.all( Model )
 
-        defmacro update(struct, opts \\ []) do
-            quote do
-                unquote(@repo).update(unquote(struct) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
-        defmacro update!(struct, opts \\ []) do
-            quote do
-                unquote(@repo).update!(unquote(struct) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
-        defmacro insert_or_update(changeset, opts \\ []) do
-            quote do
-                unquote(@repo).insert_or_update(unquote(changeset) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
-        defmacro insert_or_update!(changeset, opts \\ []) do
-            quote do
-                unquote(@repo).insert_or_update!(unquote(changeset) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
+    # therefore expands to (equivalently):
 
-        defmacro delete(struct, opts \\ []) do
-            quote do
-                unquote(@repo).delete(unquote(struct) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
-        defmacro delete!(struct, opts \\ []) do
-            quote do
-                unquote(@repo).delete!(unquote(struct) |> Multitenancy.TenantRepo.tenant_annotated_changeset_or_struct(var!(conn), unquote(@transform)), unquote(opts))
-            end
-        end
+    Repo.all( Model |> Ecto.Queryable.to_query |> Map.put(:prefix, transform.(context_variable) )
 
-        # need a use case to verify preload or insert_all
+    # where transform is a function that extracts the schema name from conn
 
+    # This module provides the ability to parameterize the key aspects of this:
+    #  Repo              - Ecto.Repo behaviour object being wrapped
+    #  Transform         - Function to obtain schema name/prefix from context_variable
+    #                        (this defaults to &Multitenancy.TenantRepo.schema_from_conn/1
+    #                            which recieves the output of the conn plugs defined in this library)
+    #  Context_variable  - Parameter bound dynamically from caller lexical scope.
+    #                        (this defaults to :conn, attaching to phoenix `conn`-based controllers)
+
+
+    # This can be configured in your application as follows:
+
+
+    # default, minimal definition, uses phoenix conn and provided plugs:
+    defmodule MyApplication.TenantRepo do
+      use Multitenancy.TenantRepo,
+        repo: UserService.Repo
     end
 
-  end
+    # configrued, if the variable "tenant_uuid" is available in calling context
+    #
+    defmodule MyApplication.TenantRepo do
+      def schema_from_uuid(uuid) do
+        "schema_" <> uuid
+      end
+      use Multitenancy.TenantRepo,
+        repo: MyApplication.Repo,
+        transform: &MyApplication.TenantRepo.schema_from_uuid/1,
+        context_variable: :tenant_uuid
+    end
+
+    """
+
+    # transform: Multitenancy.TenantRepo.schema_from_conn/1
+    def schema_from_conn(conn) do
+      conn.assigns.tenant_schema
+    end
+    # transform: Multitenancy.TenantRepo.identity/1
+    def identity(token) do
+      token
+    end
+
+    defmacro __using__(options) do
+        quote bind_quoted: [options: options] do
+            require Multitenancy.TenantRepo.RepoWrapper
+            alias Multitenancy.TenantRepo.RepoWrapper
+
+            @repo Keyword.fetch!(options, :repo)
+            @transform Keyword.fetch!(options, :transform) || &Multitenancy.TenantRepo.schema_from_conn/1
+            @context_variable Keyword.get(options, :context_variable) || :conn
+
+            @dynamic_expand [ quote do: var!(unquote(Macro.var(@context_variable,nil))) ]
+
+            defmacro all(queryable, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.all(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(opts))
+            end
+
+            defmacro get(queryable, id, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.get(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(id), unquote(opts))
+            end
+            defmacro get!(queryable, id, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.get!(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(id), unquote(opts))
+            end
+
+            defmacro get_by(queryable, clauses, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.get_by(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(clauses), unquote(opts))
+            end
+            defmacro get_by!(queryable, clauses, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.get_by!(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(clauses), unquote(opts))
+            end
+
+            defmacro one(queryable, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.one(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(opts))
+            end
+            defmacro one!(queryable, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.one!(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(opts))
+            end
+
+            defmacro aggregate(queryable, aggregate, field, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.aggregate(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(aggregate), unquote(field), unquote(opts))
+            end
+
+            # does struct annotation need to be applied to "updates"
+            defmacro update_all(queryable, updates, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.update_all(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(updates), unquote(opts))
+            end
+            defmacro delete_all(queryable, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.delete_all(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(queryable), unquote(opts))
+            end
+
+            defmacro insert(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.insert(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+            defmacro insert!(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.insert!(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+
+            defmacro update(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.update(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+            defmacro update!(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.update!(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+
+            defmacro insert_or_update(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.insert_or_update(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+            defmacro insert_or_update!(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.insert_or_update!(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+
+            defmacro delete(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.delete(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+            defmacro delete!(struct, opts \\ []) do
+                variable_splice = RepoWrapper.get_var_splice(@context_variable)
+                quote do: RepoWrapper.delete!(unquote(@repo),unquote(@transform).( unquote_splicing( variable_splice )),
+                    unquote(struct), unquote(opts))
+            end
+
+            # need a use case to verify preload or insert_all
+
+        end
+
+    end
 end
 
---
+++ lib/multitenancy/tenant_repo/repo_wrapper.ex
@@ -0,0 +1,113 @@
+defmodule Multitenancy.TenantRepo.RepoWrapper do
+
+    # consider here using ApartmentEx's implementation, however it lacks the full set of function signatures... ?
+
+
+    # wrapper methods
+    def all(repo, prefix, queryable, opts \\ []) do
+        repo.all( tenant_annotated_query(queryable, prefix), opts)
+    end
+
+    def stream(repo, prefix, queryable, opts \\ []) do
+        repo.stream( tenant_annotated_query(queryable, prefix), opts)
+    end
+
+    def get(repo, prefix, queryable, id, opts \\ []) do
+        repo.get( tenant_annotated_query(queryable, prefix), id, opts)
+    end
+
+    def get!(repo, prefix, queryable, id, opts \\ []) do
+        repo.get!( tenant_annotated_query(queryable, prefix), id, opts)
+    end
+
+    def get_by(repo, prefix, queryable, clauses, opts \\ []) do
+        repo.get_by( tenant_annotated_query(queryable, prefix), clauses, opts)
+    end
+
+    def get_by!(repo, prefix, queryable, clauses, opts \\ []) do
+        repo.get_by!( tenant_annotated_query(queryable, prefix), clauses, opts)
+    end
+
+    def one(repo, prefix, queryable, opts \\ []) do
+        repo.one( tenant_annotated_query(queryable, prefix), opts)
+    end
+
+    def one!(repo, prefix, queryable, opts \\ []) do
+        repo.one!( tenant_annotated_query(queryable, prefix), opts)
+    end
+
+    def aggregate(repo, prefix, queryable, aggregate, field, opts \\ []) do
+        repo.aggregate( tenant_annotated_query(queryable, prefix), aggregate, field, opts)
+    end
+
+    def insert_all(repo, prefix, schema_or_source, entries, opts \\ []) do
+        repo.insert_all( tenant_annotated_changeset_or_struct(schema_or_source, prefix), entries, opts)
+    end
+
+    def update_all(repo, prefix, queryable, updates, opts \\ []) do
+        repo.update_all( tenant_annotated_query(queryable, prefix), updates, opts)
+    end
+
+    def delete_all(repo, prefix, queryable, opts \\ []) do
+        repo.delete_all( tenant_annotated_query(queryable, prefix), opts)
+    end
+
+    def insert(repo, prefix, struct, opts \\ []) do
+        repo.insert( tenant_annotated_changeset_or_struct(struct, prefix), opts)
+    end
+
+    def update(repo, prefix, struct, opts \\ []) do
+        repo.update( tenant_annotated_changeset_or_struct(struct, prefix), opts)
+    end
+
+    def insert_or_update(repo, prefix, changeset, opts \\ []) do
+        repo.insert_or_update( tenant_annotated_changeset_or_struct(changeset, prefix), opts)
+    end
+
+    def delete(repo, prefix, struct, opts \\ []) do
+        repo.delete( tenant_annotated_changeset_or_struct(struct, prefix), opts)
+    end
+
+    def insert!(repo, prefix, struct, opts \\ []) do
+        repo.insert!( tenant_annotated_changeset_or_struct(struct, prefix), opts)
+    end
+
+    def update!(repo, prefix, struct, opts \\ []) do
+        repo.update!( tenant_annotated_changeset_or_struct(struct, prefix), opts)
+    end
+
+    def insert_or_update!(repo, prefix, changeset, opts \\ []) do
+        repo.insert_or_update( tenant_annotated_changeset_or_struct(changeset, prefix), opts)
+    end
+
+    def delete!(repo, prefix, struct, opts \\ []) do
+        repo.delete!( tenant_annotated_changeset_or_struct(struct, prefix), opts)
+    end
+
+
+    # helper methods
+    # various ways in which we annotate the inputs to enforce the tenant schema
+    defp tenant_annotated_query(query, prefix) do
+        query |> Ecto.Queryable.to_query |> Map.put(:prefix, prefix )
+    end
+    defp tenant_annotated_changeset(changeset, prefix) do
+        %Ecto.Changeset{changeset | data: tenant_annotated_struct(changeset.data, prefix) }
+    end
+    defp tenant_annotated_struct(struct, prefix) do
+        struct |> Ecto.put_meta(prefix: prefix)
+    end
+
+    # some helpers for inputs which may vary...
+    defp tenant_annotated_changeset_or_struct(%Ecto.Changeset{} = changeset, prefix) do
+        tenant_annotated_changeset(changeset, prefix)
+    end
+    # If it's not a changeset, assume it's a data struct
+    defp tenant_annotated_changeset_or_struct(struct, prefix) do
+        tenant_annotated_struct(struct, prefix)
+    end
+
+    def get_var_splice(context_var) do
+        [ quote do: var!(unquote(Macro.var(context_var,nil))) ]
+    end
+
+end
